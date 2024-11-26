517e7b532103bd2ee06d4d385a71e3917c35da61
jack <sahojack@gmail.com>
2024-11-26 11:29:13 +0800

    Add Project

---
+++ BlazorRadzenRestfulBookApp/Program.cs
@@ -0,0 +1,46 @@
+using BlazorRadzenRestfulBookApp.Components;
+using BlazorRadzenRestfulBookApp.Repository;
+using Radzen;
+
+namespace BlazorRadzenRestfulBookApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
+
+            builder.Services.AddScoped(SP => new HttpClient { BaseAddress = new Uri("https://localhost:5600/") });
+            builder.Services.AddScoped<IBookRepository, BookApiRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
